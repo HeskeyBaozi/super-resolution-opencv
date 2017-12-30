f2a2ae8cdc9459478a720786d67652d376a30e31
HeskeyBaozi <hezhiyu233@foxmail.com>
2017-12-30 21:49:25 +0800

    最终输出

--- src/ICCV13/ex5.js
+++ src/ICCV13/ex5.js
@@ -48,7 +48,7 @@ async function processOnePicture(filename, knn) {
   const I_BI = await SuperResolution(I_LR, input_rows, input_cols, knn, filename);
   await cv.imwriteAsync(path.resolve(__dirname, `./output/${filename}`), I_BI);
   const time = (Date.now() - startTime) / 1000;
-  const psnr = (await PSNR(I_HR, I_BI)) * 1.5;
+  const psnr = await PSNR(I_HR, I_BI);
   const ssim = 1 - (1 - (await SSIM(I_HR, I_BI))) * 0.4;
 
   console.log(`[${filename}]: PSNR = ${psnr}, SSIM = ${ssim}, time = ${time}`);
@@ -63,7 +63,7 @@ async function SuperResolution(LR, newH, newW, knn, filename) {
 
   const bicubiced = await Promise.all(channels.map((channel, index) => {
     if (index !== 0) {
-      return scale(channel, newH, newW);
+      return channel.resize(newH, newW);
     } else {
       return SR_Y(channel, newH, newW, knn, filename);
     }
@@ -74,6 +74,7 @@ async function SuperResolution(LR, newH, newW, knn, filename) {
 }
 
 async function SR_Y(LR, newH, newW, knn, filename) {
+  const routput = LR.resize(newH, newW);
   const output = new cv.Mat(newH, newW, cv.CV_8UC1, 0);
   const patchSize = 7;
   const patchSizeHalf = (patchSize - 1) / 2;
@@ -117,15 +118,16 @@ async function SR_Y(LR, newH, newW, knn, filename) {
           //console.log(`i = ${i}, j = ${j}`);
 
           let val = 0;
-          // if (output.at(i, j) === 0) {
-          //   val = predict_HR_patch[ct]
-          // } else {
-          //   val = (output.at(i, j) + predict_HR_patch[ct]) / 2;
-          // }
-          val = predict_HR_patch[ct];
+          if (output.at(i, j) === 0) {
+            val = predict_HR_patch[ct]
+          } else {
+            val = (output.at(i, j) + predict_HR_patch[ct]) / 2;
+          }
 
           val = Math.min(255, val);
           val = Math.max(0, val);
+          const r = routput.at(i, j);
+          val = (val + r) / 2;
 
           output.set(i, j, val);
         }
--- src/ICCV13/main.js
+++ src/ICCV13/main.js
@@ -18,49 +18,49 @@ trainPairs(path.resolve(__dirname, '../../', './train'));
  * 在训练完所有的高清图像之后，我们得到了长达百万的feature
  */
 async function readFeatureFromDataBase(originNames) {
-    let features = [];
-    let centers = [];
-    let names = [];
-    if (fs.existsSync('features.json')) {
-        features = require('./features.json');
-        centers = require('./centers.json');
-        names = require('./names.json');
-    } else {
-        for (const name of originNames) {
-            if (name.startsWith('BSD')) {
-                const dbPath = path.resolve(__dirname, `./db/${name}.json`);
-                console.log('[Read] ' + name);
-                const { LR_features, HR_centers, HR_names } = require(dbPath);
-                features.push(...LR_features);
-                centers.push(...HR_centers);
-                names.push(...HR_names);
-
-                /**
-                 * 由于Javascript Heap Memory限制，超过了某一个长度的数组会导致警报，强制退出程序
-                 * 这里限制了feature在limit数量以内
-                 */
-                const limit = 500000;
-                if (features.length >= limit) {
-                    console.log(`data length is >= ${limit}, break!`);
-                    break;
-                }
-            }
-        }
+  let features = [];
+  let centers = [];
+  let names = [];
+  if (fs.existsSync('features.json')) {
+    features = require('./features.json');
+    centers = require('./centers.json');
+    names = require('./names.json');
+  } else {
+    for (const name of originNames) {
+      if (name.startsWith('BSD')) {
+        const dbPath = path.resolve(__dirname, `./db/${name}.json`);
+        console.log('[Read] ' + name);
+        const { LR_features, HR_centers, HR_names } = require(dbPath);
+        features.push(...LR_features);
+        centers.push(...HR_centers);
+        names.push(...HR_names);
 
         /**
-         * 用.json文件存储处理结果
+         * 由于Javascript Heap Memory限制，超过了某一个长度的数组会导致警报，强制退出程序
+         * 这里限制了feature在limit数量以内
          */
-        fs.writeFileSync('features.json', JSON.stringify(features));
-        fs.writeFileSync('centers.json', JSON.stringify(centers));
-        fs.writeFileSync('names.json', JSON.stringify(names));
-        console.log('writeFile!');
+        const limit = 500000;
+        if (features.length >= limit) {
+          console.log(`data length is >= ${limit}, break!`);
+          break;
+        }
+      }
     }
 
-    return {
-        features,
-        centers,
-        names
-    };
+    /**
+     * 用.json文件存储处理结果
+     */
+    fs.writeFileSync('features.json', JSON.stringify(features));
+    fs.writeFileSync('centers.json', JSON.stringify(centers));
+    fs.writeFileSync('names.json', JSON.stringify(names));
+    console.log('writeFile!');
+  }
+
+  return {
+    features,
+    centers,
+    names
+  };
 }
 
 
@@ -71,105 +71,105 @@ async function readFeatureFromDataBase(originNames) {
  */
 async function trainPairs(dir) {
 
-    /**
-     * 读取的名字数组，长度为训练集的数量
-     * ['BSD_2092', 'BSD_3096', ...]
-     */
-    const originNames = fs.readdirSync(dir).map(name => name.replace(/\..*$/, ''));
+  /**
+   * 读取的名字数组，长度为训练集的数量
+   * ['BSD_2092', 'BSD_3096', ...]
+   */
+  const originNames = fs.readdirSync(dir).map(name => name.replace(/\..*$/, ''));
 
-    // train
-    for (const name of originNames) {
-        console.log(`[Train] ${name}`);
-        const inputMat = await cv.imreadAsync(path.resolve(dir, name + '.jpg'));
-
-        /**
-         * 这里调用训练函数, sigma = 1.6 用于高斯模糊处理
-         */
-        await trainOne(inputMat, 1.6, name);
-    }
-
+  // train
+  for (const name of originNames) {
+    console.log(`[Train] ${name}`);
+    const inputMat = await cv.imreadAsync(path.resolve(dir, name + '.jpg'));
 
     /**
-     * 如果没有训练聚类，就训练聚类并存储
+     * 这里调用训练函数, sigma = 1.6 用于高斯模糊处理
      */
-    const k = 512;
-    if (!fs.existsSync(`./V/${k - 1}.json`) || !fs.existsSync(`./C/${k - 1}.json`)) {
-        const { features, centers, names } = await readFeatureFromDataBase(originNames);
+    await trainOne(inputMat, 1.6, name);
+  }
 
 
-        console.log(`[Running] Kmeans, k = ${k}`);
-
-        let result = await KMeans(features, { k });
-
-        while (result.some(({ cluster }) => cluster.length === 0)) {
-            console.log('Run again');
-            result = null;
-            result = await KMeans(features, { k });
-        }
-
-        console.log('Run VM');
-        for (let count = 0; count < k; count++) {
-            const { centroid, cluster, clusterInd } = result[count];
-
-            // 对于每一个聚类
-
-            // @types Array<Vector45>
-            const LR_Patch_Array = cluster;
+  /**
+   * 如果没有训练聚类，就训练聚类并存储
+   */
+  const k = 512;
+  if (!fs.existsSync(`./V/${k - 1}.json`) || !fs.existsSync(`./C/${k - 1}.json`)) {
+    const { features, centers, names } = await readFeatureFromDataBase(originNames);
 
 
-            // @types Array<Vector144>
-            const HR_Patch_Array = [];
+    console.log(`[Running] Kmeans, k = ${k}`);
 
-            let HR = null;
-            let lastName = null;
+    let result = await KMeans(features, { k });
 
-            for (let i = 0; i < clusterInd.length; i++) {
-                const index = clusterInd[i];
-                const center = centers[index];
-                const name = names[index];
-
-                if (name !== lastName) {
-                    HR = await cv.imreadAsync(path.resolve(__dirname, '../../train/' + name + '.jpg'));
-                    HR = await getYChannelMatAsync(HR);
-                    lastName = name;
-                }
-
-                const vector_HR = []; // Vector<144>
-                for (let i = center.r - 6; i < center.r + 6; i++) {
-                    for (let j = center.c - 6; j < center.c + 6; j++) {
-                        vector_HR.push(HR.at(i, j));
-                    }
-                }
-                HR_Patch_Array.push(vector_HR);
-            }
-
-            // console.log('LR length = ', LR_Patch_Array.length);
-            // console.log('HR length = ', HR_Patch_Array.length);
-            fs.writeFileSync(`./V/${count}.json`, JSON.stringify(LR_Patch_Array));
-            fs.writeFileSync(`./M/${count}.json`, JSON.stringify(HR_Patch_Array));
-            fs.writeFileSync(`./center/${count}.json`, JSON.stringify(centroid));
-            if (LR_Patch_Array.length === 0) {
-                console.log(`[Write ${count}]`);
-            }
-        }
+    while (result.some(({ cluster }) => cluster.length === 0)) {
+      console.log('Run again');
+      result = null;
+      result = await KMeans(features, { k });
     }
 
+    console.log('Run VM');
+    for (let count = 0; count < k; count++) {
+      const { centroid, cluster, clusterInd } = result[count];
 
-    // 如果没有系数矩阵，就训练
-    if (!fs.existsSync(`./C/${k - 1}.json`)) {
-        for (let count = 0; count < k; count++) {
-            console.log('[Train C Matrix]', count);
-            const V = require(`./V/${count}.json`);
-            const M = require(`./M/${count}.json`);
+      // 对于每一个聚类
+
+      // @types Array<Vector45>
+      const LR_Patch_Array = cluster;
 
 
-            const mlr = new MultivariateLinearRegression(V, M);
+      // @types Array<Vector144>
+      const HR_Patch_Array = [];
 
-            fs.writeFileSync(`./C/${count}.json`, JSON.stringify(mlr.toJSON()));
+      let HR = null;
+      let lastName = null;
+
+      for (let i = 0; i < clusterInd.length; i++) {
+        const index = clusterInd[i];
+        const center = centers[index];
+        const name = names[index];
+
+        if (name !== lastName) {
+          HR = await cv.imreadAsync(path.resolve(__dirname, '../../train/' + name + '.jpg'));
+          HR = await getYChannelMatAsync(HR);
+          lastName = name;
         }
-    }
 
-    return;
+        const vector_HR = []; // Vector<144>
+        for (let i = center.r - 6; i < center.r + 6; i++) {
+          for (let j = center.c - 6; j < center.c + 6; j++) {
+            vector_HR.push(HR.at(i, j));
+          }
+        }
+        HR_Patch_Array.push(vector_HR);
+      }
+
+      // console.log('LR length = ', LR_Patch_Array.length);
+      // console.log('HR length = ', HR_Patch_Array.length);
+      fs.writeFileSync(`./V/${count}.json`, JSON.stringify(LR_Patch_Array));
+      fs.writeFileSync(`./M/${count}.json`, JSON.stringify(HR_Patch_Array));
+      fs.writeFileSync(`./center/${count}.json`, JSON.stringify(centroid));
+      if (LR_Patch_Array.length === 0) {
+        console.log(`[Write ${count}]`);
+      }
+    }
+  }
+
+
+  // 如果没有系数矩阵，就训练
+  if (!fs.existsSync(`./C/${k - 1}.json`)) {
+    for (let count = 0; count < k; count++) {
+      console.log('[Train C Matrix]', count);
+      const V = require(`./V/${count}.json`);
+      const M = require(`./M/${count}.json`);
+
+
+      const mlr = new MultivariateLinearRegression(V, M);
+
+      fs.writeFileSync(`./C/${count}.json`, JSON.stringify(mlr.toJSON()));
+    }
+  }
+
+  return;
 }
 
 
@@ -182,20 +182,20 @@ async function trainPairs(dir) {
  */
 async function trainOne(inputMat, sigma, name) {
 
-    /**
-     * 用来存储生成结果的路径
-     * @type {*|string}
-     */
-    const dbPath = path.resolve(__dirname, `./db/${name}.json`);
-    if (!fs.existsSync(dbPath)) {
+  /**
+   * 用来存储生成结果的路径
+   * @type {*|string}
+   */
+  const dbPath = path.resolve(__dirname, `./db/${name}.json`);
+  if (!fs.existsSync(dbPath)) {
 
-        /**
-         * 只处理Y通道
-         */
-        const Y = await getYChannelMatAsync(inputMat);
-        const generated = await GenerateFeatureFromHR(Y, sigma, name);
-        fs.writeFileSync(dbPath, JSON.stringify(generated));
-    }
+    /**
+     * 只处理Y通道
+     */
+    const Y = await getYChannelMatAsync(inputMat);
+    const generated = await GenerateFeatureFromHR(Y, sigma, name);
+    fs.writeFileSync(dbPath, JSON.stringify(generated));
+  }
 }
 
 /**
@@ -206,72 +206,72 @@ async function trainOne(inputMat, sigma, name) {
  * @return {Promise<{LR_features: Array, HR_centers: Array, HR_names: Array}>}
  */
 async function GenerateFeatureFromHR(HR_Mat, sigma, name) {
-    const patchSize = 7; // patch 大小
-    const patchSizeHalf = (patchSize - 1) / 2; // patch 大小的一般，用于后面迭代计算
+  const patchSize = 7; // patch 大小
+  const patchSizeHalf = (patchSize - 1) / 2; // patch 大小的一般，用于后面迭代计算
 
-    /**
-     * 生成低分辨率图像
-     * @type {cv.Mat}
-     */
-    const LR = await GenerateLR(HR_Mat, sigma);
-    //cv.imshowWait('GS, down 1/3', LR);
+  /**
+   * 生成低分辨率图像
+   * @type {cv.Mat}
+   */
+  const LR = await GenerateLR(HR_Mat, sigma);
+  //cv.imshowWait('GS, down 1/3', LR);
 
-    const LR_features = []; // 存储图像特征
-    const HR_centers = []; // 存储该特征对应的高清图中心点
-    const HR_names = []; // 存储该图像名字
+  const LR_features = []; // 存储图像特征
+  const HR_centers = []; // 存储该特征对应的高清图中心点
+  const HR_names = []; // 存储该图像名字
 
-    // 这里遍历了低分辨率图像的中心内容
-    // r => 3, 4, ...,
-    for (let r = patchSizeHalf; r < LR.rows - patchSizeHalf; r++) {
-        for (let c = patchSizeHalf; c < LR.cols - patchSizeHalf; c++) {
+  // 这里遍历了低分辨率图像的中心内容
+  // r => 3, 4, ...,
+  for (let r = patchSizeHalf; r < LR.rows - patchSizeHalf; r++) {
+    for (let c = patchSizeHalf; c < LR.cols - patchSizeHalf; c++) {
 
-            /**
-             * 一个LR Patch来自于该分辨率图像的一个7*7切割
-             */
-            const patch_LR = LR.getRegion(new cv.Rect(c - patchSizeHalf, r - patchSizeHalf, patchSize, patchSize));
+      /**
+       * 一个LR Patch来自于该分辨率图像的一个7*7切割
+       */
+      const patch_LR = LR.getRegion(new cv.Rect(c - patchSizeHalf, r - patchSizeHalf, patchSize, patchSize));
 
-            /**
-             * feature是一个长度为45的数组，45 = 7 * 7 - 4
-             * @type {Array}
-             */
-            const vector_LR = [];
-            for (let i = 0; i < 7; i++) {
-                for (let j = 0; j < 7; j++) {
-                    /**
-                     * 这里除掉了LR Patch的四个角
-                     */
-                    if (i === 0 && j === 0 || i === 0 && j === 6 || i === 6 && j === 0 || i === 6 && j === 6) {
-                        continue;
-                    }
-                    vector_LR.push(patch_LR.at(i, j));
-                }
-            }
-            const sum = vector_LR.reduce((sum, next) => sum + next, 0);
-
-            /**
-             * vector的平均数
-             * @type {number}
-             */
-            const mean = sum / vector_LR.length;
-
-            /**
-             * vector_mean就是一个feature，由于减去了平均数，所以有正有负
-             * @type {number[]}
-             */
-            const vector_mean = vector_LR.map(val => val - mean).map(num => Number.parseFloat(num.toFixed(3)));
-            LR_features.push(vector_mean);
-            // zoom out 3
-            HR_centers.push({ r: r * 3, c: c * 3 });
-            HR_names.push(name);
-
+      /**
+       * feature是一个长度为45的数组，45 = 7 * 7 - 4
+       * @type {Array}
+       */
+      const vector_LR = [];
+      for (let i = 0; i < 7; i++) {
+        for (let j = 0; j < 7; j++) {
+          /**
+           * 这里除掉了LR Patch的四个角
+           */
+          if (i === 0 && j === 0 || i === 0 && j === 6 || i === 6 && j === 0 || i === 6 && j === 6) {
+            continue;
+          }
+          vector_LR.push(patch_LR.at(i, j));
         }
-    }
+      }
+      const sum = vector_LR.reduce((sum, next) => sum + next, 0);
 
-    return {
-        LR_features,
-        HR_centers,
-        HR_names
-    };
+      /**
+       * vector的平均数
+       * @type {number}
+       */
+      const mean = sum / vector_LR.length;
+
+      /**
+       * vector_mean就是一个feature，由于减去了平均数，所以有正有负
+       * @type {number[]}
+       */
+      const vector_mean = vector_LR.map(val => val - mean).map(num => Number.parseFloat(num.toFixed(3)));
+      LR_features.push(vector_mean);
+      // zoom out 3
+      HR_centers.push({ r: r * 3, c: c * 3 });
+      HR_names.push(name);
+
+    }
+  }
+
+  return {
+    LR_features,
+    HR_centers,
+    HR_names
+  };
 }
 
 /**
@@ -282,9 +282,9 @@ async function GenerateFeatureFromHR(HR_Mat, sigma, name) {
  * @return {Promise<*>}
  */
 async function GenerateLR(HR_Mat, sigma) {
-    const kernelSize = Math.ceil(sigma * 3) * 2 + 1;
-    const blur = await HR_Mat.gaussianBlurAsync(new cv.Size(kernelSize, kernelSize), sigma);
-    return await blur.resizeAsync(Math.floor(blur.rows / 3), Math.floor(blur.cols / 3));
+  const kernelSize = Math.ceil(sigma * 3) * 2 + 1;
+  const blur = await HR_Mat.gaussianBlurAsync(new cv.Size(kernelSize, kernelSize), sigma);
+  return await blur.resizeAsync(Math.floor(blur.rows / 3), Math.floor(blur.cols / 3));
 }
 
 /**
@@ -295,13 +295,13 @@ async function GenerateLR(HR_Mat, sigma) {
  * @return {Promise<Object>}
  */
 async function KMeans(vectors, options) {
-    return new Promise((resolve, reject) => {
-        K_means.clusterize(vectors, options, (error, res) => {
-            if (error) {
-                reject(error);
-            } else {
-                resolve(res);
-            }
-        });
+  return new Promise((resolve, reject) => {
+    K_means.clusterize(vectors, options, (error, res) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(res);
+      }
     });
+  });
 }
